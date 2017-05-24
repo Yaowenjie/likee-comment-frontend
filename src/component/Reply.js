3e9e6ad5943d698df83c2932daad312a1ed8c91c
Yaowenjie <wjyao@thoughtworks.com>
2017-05-24 12:56:41 +0800

    make letter in avator center

--- src/component/Reply.js
+++ src/component/Reply.js
@@ -12,7 +12,7 @@ class Reply extends Component {
   render() {
     return (
       <div className={styles.main}>
-        <Avator letter="我" size={40} color={"#a3d4b3"} />
+        <Avator letter="T" size={40} color={"#a3d4b3"} />
         {/*<img src="http://q.qlogo.cn/qqapp/100229475/45930EFEC169CA93E35A4D4ADAE480AC/100" alt="竹影清风" className={styles.avator}/>*/}
         <div className={styles.info}>
           <div className={styles.head}>
